dec7b846bd79ea32b3049e341ee2c7c495e6648d
zane <neuschuler@gmail.com>
2023-09-11 20:35:30 -0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,48 @@
+import discord
+import aiohttp
+import io
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = discord.Bot()
+bot_guild_ids = guild_ids=["1150971174805844139"]
+
+
+# create Slash Command group with bot.create_group
+greetings = bot.create_group("greetings", "Greet people")
+
+@greetings.command()
+async def hello(ctx):
+  await ctx.respond(f"Hello, {ctx.author}!")
+
+@greetings.command()
+async def bye(ctx):
+  await ctx.respond(f"Bye, {ctx.author.id}!")
+
+santa = bot.create_group("santa", "Secret santa things!")
+
+@santa.command(description="Upload your skeleton!")
+async def upload(ctx, file:discord.Attachment, stepartist:discord.SlashCommandOptionType.string):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(file.url) as resp:
+            if resp.status != 200:
+                return await ctx.send('Could not download file...')
+            data = io.BytesIO(await resp.read())
+            upload_ext = file.url.split(".")[-1]
+            
+            f = open(f'uploads/{ctx.author.id}.{upload_ext}', "wb")
+            f.write(data.getbuffer())
+            f.close()
+            await ctx.respond(file=discord.File(data, f'{ctx.author.id}.{upload_ext}'), content=f'Hi {stepartist}!')
+
+@santa.command(description="LET'S FUCKING GOOOOOOOOOOOOOOOOOO")
+async def hohoho(ctx):
+    print(type(ctx.author.id))
+    if (ctx.author.id != 262440960040894474) or (ctx.author.id != 84108714671345664):
+        await ctx.respond("You're not allowed to start the christmas season!")
+    else:
+        await ctx.respond("Ho ho ho! Be prepared to get your files...")
+
+bot.run(os.getenv("CLIENT_TOKEN"))
