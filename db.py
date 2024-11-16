e60fef682fbb1ed36a59fb44d97c308e70485a8a
zane <neuschuler@gmail.com>
2024-11-16 12:36:44 -0800

    Added initializing db if it does not exist/has no columns.

--- db.py
+++ db.py
@@ -5,7 +5,10 @@ def put(discord_id, stepartist, database):
     db = sqlite3.connect(database)
     cur = db.cursor()
     data = (discord_id, stepartist)
-    cur.execute(f"SELECT discord_id FROM stepartists WHERE discord_id ={discord_id}")
+    try:
+        cur.execute(f"SELECT discord_id FROM stepartists WHERE discord_id ={discord_id}")
+    except sqlite3.OperationalError:
+        init(db)
     result = cur.fetchone()
     if result:
         print("User already in database! Skipping...")
@@ -21,7 +24,10 @@ def put(discord_id, stepartist, database):
 def get(database):
     db = sqlite3.connect(database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM stepartists")
+    try:
+        cur.execute("SELECT * FROM stepartists")
+    except sqlite3.OperationalError:
+        init(db)
     fetched = cur.fetchall()
     print(type(fetched))
     db.close()
@@ -29,7 +35,14 @@ def get(database):
 
 def wipe(database):
     db = sqlite3.connect(database)
-    db.execute("DELETE FROM stepartists")
+    try:
+        db.execute("DELETE FROM stepartists")
+    except sqlite3.OperationalError:
+        init(db)
     db.commit()
     db.close()
     print("DB should be wiped! Make sure to check though...")
+
+
+def init(db):
+    db.execute("CREATE TABLE IF NOT EXISTS stepartists (discord_id INT, stepartist_name TEXT)")
